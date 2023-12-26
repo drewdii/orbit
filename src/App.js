3bd161d13da74275178b5ae561fa4f59c13f8087
drewdi <realandrewhunter@gmail.com>
2023-12-26 03:12:14 -0600

    more changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import {
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import LinkComponent from './Components/LinkComponent';
-import { Logo } from './Logo';
 
 function App() {
   return (
@@ -20,7 +19,7 @@ function App() {
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
-          <Flex w="100%" justifyContent={center}>
+          <Flex w="100%" justifyContent="center">
             <LinkComponent image="INSERT YOUR PFP.png" />
           </Flex>
         </Grid>
