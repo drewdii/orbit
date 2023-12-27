424fc9cf42d45db14b3c0a2bd8f711310f03f2ea
drewdi <realandrewhunter@gmail.com>
2023-12-27 12:36:12 -0600

    big push of changes

--- src/App.js
+++ src/App.js
@@ -8,15 +8,21 @@ import {
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import LinkComponent from './Components/LinkComponent';
+import {AiFillGithub} from 'react-icons/ai'
 
 function App() {
+
+  const links = [
+      {url: "https://github.com", icon: <AiFillGithub/>, color1: "gray.200", color2: "blue.400"}
+  ]
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <Flex w="100%" justifyContent="center">
-            <LinkComponent image="./logo512.png" />
+            <LinkComponent image="./logo512.png" links={index} />
           </Flex>
         </Grid>
       </Box>
--- src/Components/LinkComponent.js
+++ src/Components/LinkComponent.js
@@ -1,13 +1,58 @@
 import { Box, Image } from "@chakra-ui/react"
 
-const LinkComponent = ({image}) => {
+const LinkComponent = ({ image, links }) => {
     return (
-        <Box h="500px" w="500px" display="grid" placeContent="center" transform={["scale(0.9)", null, "scale(1)", null]}>
-        <Box w="200px" h="200px" display="grid" placeContent="center" borderRadius="100%" dropShadow="0px 0px 30px rgba(0,0,0,0.5)">
-            <Image w="150px" h="150px" src={image} alt="center image"/>
+        <Box 
+          h="500px" 
+          w="500px"
+          display="grid"
+          placeContent="center"
+          transform={["scale(0.9)", null, "scale(1)", null]}
+        >
+            <Box 
+              w="200px"
+              h="200px"
+              display="grid"
+              placeContent="center"
+              borderRadius="100%" 
+              dropShadow="0px 0px 30px rgba(0,0,0,0.5)"
+            >
+              <Image w="150px" h="150px" src={image} alt="center image"/>
+            </Box>
+            <Box position="absolute" w="100%" h="100%">
+                {links.map((link, index) => (
+                    <ButtonComponent link={link} index={index}/>
+                ))}
+            </Box>
         </Box>
+    );
+};
+
+const ButtonComponent = ({ link, index }) => {
+    return(
+        <Box w="100px" top="200px" left="200px" position="absolute">
+            <Box w="100%" h="100%" position="relative">
+                <a href={link.url} target="_blank" rel="noferrer">
+                    <Box 
+                      boxShadow="0px 0px 20px rgbd(0,0,0,0.5)" 
+                      display="grid"
+                      placeContent="center"
+                      w="75px"
+                      h="75px"
+                      position="absolute"
+                      borderRadius="100%"
+                      left="-110px"
+                      top="-110px"
+                      bgGradient={'linear(to-tl, ${linl.color1}, ${link.color2})'}
+                    >
+                        <Box color="white" fontsize={35}>
+                            {link.icon}
+                        </Box>
+                    </Box>
+                </a>
+            </Box>
         </Box>
-    )
+    );
 }
 
-export default LinkComponent
+export default LinkComponent;
